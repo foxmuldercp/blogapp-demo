ed75060c6120928dfd913d9ba5653f9daa43d455
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2018-02-15 23:07:48 +0200

    routes moved to /api scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :categories
-  resources :users do
-    collection do
-      post 'login'
+  scope 'api' do
+    resources :categories
+    resources :users do
+      collection do
+        post 'login'
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
