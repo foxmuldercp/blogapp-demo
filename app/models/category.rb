f2a03463109e8ba5e2559f083f4ac79f5e45b8a6
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2018-02-15 20:43:47 +0200

    Name validation regex fixed up to required minimum of 2 words

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
-  validates_format_of :name, :with => /\A([A-Za-z]{2,})\ ([A-Za-z]{2,})\Z/i
+  validates_format_of :name, :with => /\A([A-Za-z]{2,})\ ([A-Za-z]{2,})/i
 
   before_save :capitalize_name
 
