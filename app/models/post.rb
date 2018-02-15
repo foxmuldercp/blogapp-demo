298c9f9f8005f863c0c5dc890b504aec2835364d
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2018-02-16 00:21:35 +0200

    Add post name regex validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ApplicationRecord
+
   belongs_to :category
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   mount_uploader :file, AttachmentUploader
   validates :file, file_size: { less_than: 2.megabytes }
+  validates_format_of :name, :with => /\A([A-Za-z]{2,})\ ([A-Za-z]{2,})/i
+
 end
