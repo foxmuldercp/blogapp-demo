505bf767cefdbd14bc4cc1c608d0dde9f4596b95
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2018-02-19 22:15:45 +0200

    Comment url/api path fixed by adding self.post part

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,11 +6,11 @@ class Comment < ApplicationRecord
   validates_format_of :author, :with => /\A([A-Za-z]{2,})\ ([A-Za-z]{2,})/i
 
   def url
-    post_comments_path(self).remove('/api')
+    post_comment_path(self.post, self).remove('/api')
   end
 
   def api_path
-    post_comments_path(self)
+    post_comment_path(self.post, self)
   end
 
 end
